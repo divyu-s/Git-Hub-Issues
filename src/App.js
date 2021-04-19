1ff7bc6c81e179e1290fa0248458b5b9d64eae76
DivyuOpenSource <shukladivyanshu745893@gmail.com>
2021-04-19 23:57:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import Header from "./components/Header/";
+import {useState} from "react";
+import { BrowserRouter as Router, Switch ,Route} from "react-router-dom";
+import IssueTable from './components/IssueTable/';
 import './App.css';
 
+
+const API = "https://api.github.com/repos/${username}/${reponame}/issues";
 function App() {
+  const [res, setRes] = useState([]);
+  const [error,setError] = useState("");
+  const [flag,setFlag] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+             <Header setRes={setRes} setError={setError} setFlag={setFlag}/>
+             <IssueTable res={res} error={error} flag={flag}/>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/IssueTable/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import "./IssueTable.css";
+import {useState} from "react";
+import { useHistory } from 'react-router';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+import ReactPaginate from "react-paginate";
+import img from '../../Octocat.png';
+export default function IssueTable({res,error,flag}) {
+    const [pageNumber, setPageNumber] = useState(0);
+    const visitedUsers = pageNumber*10;
+
+    const displaydata = res.slice(visitedUsers,visitedUsers+10).map((item)=>(
+        <div className="issues">
+            <ErrorOutlineIcon className="issue__logo"/>
+            <a  style={{textDecoration: "none", color: "black"}} href={item.html_url}>
+            <div className="issue__content">
+                <h3 className="issue__title">{item.title}</h3>
+                <p className="issue__detail">{`#${item.number} opened on ${item.updated_at.slice(0,10).replaceAll("-","/")} by ${item.user.login}`}</p>
+            </div>
+            </a>
+
+        </div>
+    ))
+
+    const changePage = ({selected})=>{
+        setPageNumber(selected);
+    }
+
+    return (
+       <div className="issue__table"> 
+         {
+            error?<div className="error__messgae">{error}</div>:
+           flag?
+           <>
+           <h1 className="issues__heading">Git Hub Issues's</h1>
+          { res.length>0?
+            <div className="data">{displaydata}
+            <ReactPaginate
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={Math.ceil(res.length/10)}
+            onPageChange={changePage}
+            containerClassName={"paginationBttns"}
+            previousLinkClassName={"previousBttn"}
+            nextLinkClassName={"nextBttn"}
+            disabledClassName={"paginationDisabled"}
+            activeClassName={"paginationActive"}
+            />
+            </div>            :
+            <div className="data__0"><h3>{res.length} Git Hub Issue's</h3></div>
+          }
+            </>
+            :<img className="cat__image" src={img}/>
+         }
+
+       </div>
+        
+     
+    )
+}
